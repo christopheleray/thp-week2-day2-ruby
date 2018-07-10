e7426e0a21a4042c3afa7fdcf43d36dad9e196ba
christopheleray <christophe.leray1@gmail.com>
2018-07-10 17:47:45 +0200

    fonction trader_du_dimanche

--- Exercice3/stock_picker.rb
+++ Exercice3/stock_picker.rb
@@ -1,7 +1,17 @@
 def trader_du_dimanche(array)
-  tab = array.min
-  tab << array.max
-  return array
+	margin  = 0
+	purchase_day = 0
+	selling_day  = 0
+
+	for i in (0..array.length-1)
+		for j in (i..array.length-1)
+			if array[j] - array[i] > margin
+			margin = array[j] - array[i]
+			purchase_day = i
+			selling_day = j
+		  end
+	  end
+  end
+  return total = [purchase_day, selling_day]
 end
 
-puts trader_du_dimanche([17,3,6,9,15,8,6,1,10])
--- Exercice3/stock_picker_spec.rb
+++ Exercice3/stock_picker_spec.rb
@@ -1,7 +1,7 @@
 require "stock_picker"
 
-describe "" do
-  it ""
-    expect().to eq()
+describe "la methode trader_du_dimanche" do
+  it "return the index number of the minimum and maximum value" do
+    expect(trader_du_dimanche([17,3,6,9,15,8,6,1,10])).to eq([1,4])
   end
 end
